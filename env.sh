c5a1f7d3b4cae6310bd6c0d84fdcc29fa0b40b78
arlly <arlly1003@gmail.com>
2017-12-24 12:49:04 +0900

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,5 +3,5 @@
 alias docker_project-db='docker exec -it jp-ten-db mysql -p -u root'
 alias docker_project-dbdump='docker exec -it jp-ten-db mysqldump -p -u root your_database'
 
-alias jp-ten-dbbash='docker exec -it docker_project-db bash'
-alias jp-ten-webbash='docker exec -it docker_project-web bash'
+alias docker_project-dbbash='docker exec -it docker_project-db bash'
+alias docker_project-webbash='docker exec -it docker_project-web bash'
